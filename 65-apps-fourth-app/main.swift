dd733f50213fd1e4787e9a8f130fd5fc4d93f4d0
miroma21 <miromax21@gmail.com>
2019-02-24 17:50:50 +0400

    Initial Commit

---
+++ 65-apps-fourth-app/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  65-apps-fourth-app
+//
+//  Created by Princess Max on 24.02.2019.
+//  Copyright © 2019 Princess Max. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
